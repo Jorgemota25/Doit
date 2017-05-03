9b9106e1a4149f9fe18d40dee88517a24f5fee66
Jorgemota25 <jorgemota25@gmail.com>
2017-05-03 22:43:16 +0100

    App working, missing core data

---
+++ Doit/CompleteTaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CompleteTaskViewController.swift
+//  Doit
+//
+//  Created by Jorge Mota on 03/05/17.
+//  Copyright © 2017 Jorge Mota. All rights reserved.
+//
+
+import UIKit
+
+class CompleteTaskViewController: UIViewController {
+    
+    @IBOutlet weak var taskLabel: UILabel!
+    var task = Task()
+    var previousVC = TasksViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if task.important {
+            taskLabel?.text = "❗️\(task.name)"
+        } else {
+            taskLabel?.text = task.name
+            
+        }
+            }
+
+    @IBAction func completeTapped(_ sender: Any) {
+        
+        previousVC.tasks.remove(at: previousVC.selectedIndex)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+        
+    }
+   }
---
+++ Doit/CreateTaskViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by Jorge Mota on 03/05/17.
+//  Copyright © 2017 Jorge Mota. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        // Create a task from the outlet information
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        // Add new task to array in previous view controller
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+    }
+}
